0b0e2a17bf18ee17c53678ce755dd2f2408ee855
ucfnnbx <yining.an.22@ucl.ac.uk>
2023-06-27 15:23:41 +0100

    Revert "Add materials"
    This reverts commit d263757cff6aea8c2b58a1c94de24bd48d04c7ee.

--- Code/NodeJsAPIConverter/utils/jsonStringToArray.js
+++
@@ -1,8 +0,0 @@
-/**
- * Parse stringified json object to Array
- * @param {String} string - stringified json
- * @returns converted Array
- */
-export default function jsonStringToArray(string){
-    return JSON.parse("[" + string + "]");
-}
